2764fa9dbcbd363d89faab5647f5039b5bca7e13
Hina Rao <hinrao96@gmail.com>
2023-05-17 11:17:02 -0700

    Initial commit

---
+++ usecontext/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Statements from "./components/statements";
+import "./components/App.css";
+function App() {
+  return (
+    <div className="headings">
+      <h1>Assignment 7 </h1>
+      <Statements />
+    </div>
+  );
+}
+
+export default App;
